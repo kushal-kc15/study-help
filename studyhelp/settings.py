263d8a3281e3844b8cb969a659c3c86238767a8f
kushal-kc15 <kckushal164@gmail.com>
2026-08-08 15:54:23 +0545

    Set conn_max_age to 0 to prevent database connection exhaustion

--- studyhelp/settings.py
+++ studyhelp/settings.py
@@ -97,7 +97,7 @@ if os.environ.get('DATABASE_URL'):
     DATABASES = {
         'default': dj_database_url.config(
             default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600,
+            conn_max_age=0,
             ssl_require=True
         )
     }
